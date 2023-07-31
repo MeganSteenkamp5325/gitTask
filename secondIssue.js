8697b5c4bbfb045218bde8108fd4efc796ef4d50
Megan Steenkamp <megansteenkamp04@gmail.com>
2023-07-31 10:45:35 +0200

    Added comments too secondIssue.js

---
+++ secondIssue.js
@@ -0,0 +1,7 @@
+let userData= prompt("Add a greeting in your language!")
+let userLanguage= prompt("What language was that?")
+
+console.log(`Greeting:${userData}`)
+console.log(`Language:${userLanguage}`)
+
+alert(`${userData}, ${userLanguage}.`)
